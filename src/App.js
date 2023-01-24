ae1055d352778414513e597feafb514c96577991
Pa Amat Jow <paamatjow@gmail.com>
2023-01-24 16:59:30 +0000

    5. Styled AboutSection with styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+// GLobal Style
+import GlobalStyle from './components/GlobalStyle';
 // import pages
 import AboutUs from './pages/AboutUs'
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AboutUs/>
     </div>
   );

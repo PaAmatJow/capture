c6cc72636c5afbafb1d76eeecfd66611cbc5e557
Pa Amat Jow <paamatjow@gmail.com>
2023-01-24 21:55:19 +0000

    7. Styled Faq Section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,47 +1,97 @@
-import React from 'react'
+import React, {useState} from 'react'
+// styles
+import styled from 'styled-components'
+import { About } from '../styles'
 
 const FaqSection = () => {
+  const [show, setShow] = useState(false)
   return (
-    <div className="faq">
-      <h2>Any Questions <span>FAQ</span></h2>
-      {/* single question */}
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, in.</p>
-        </div>
-      </div>
-      {/* end of single question */}
-      {/* single question */}
-      <div className="question">
-        <h4>Daaily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, in.</p>
-        </div>
-      </div>
-      {/* end of single question */}
-      {/* single question */}
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, in.</p>
-        </div>
-      </div>
-      {/* end of single question */}
-      {/* single question */}
-      <div className="question">
-        <h4>What Products Do You Offer?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, in.</p>
-        </div>
-      </div>
-      {/* end of single question */}
-    </div>
-  )
+		<Faq>
+			<h2>
+				Any Questions <span>FAQ</span>
+			</h2>
+			{/* single question */}
+			<div className='question' onClick={()=>setShow(!show)}>
+				<h4>How Do I Start?</h4>
+				{show && (
+					<div className='answer'>
+						<p>Lorem ipsum dolor sit amet.</p>
+						<p>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque,
+							in.
+						</p>
+					</div>
+				)}
+				<div className='faq-line'></div>
+			</div>
+			{/* end of single question */}
+			{/* single question */}
+			<div className='question'>
+				<h4>Daily Schedule</h4>
+				<div className='answer'>
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque,
+						in.
+					</p>
+				</div>
+				<div className='faq-line'></div>
+			</div>
+			{/* end of single question */}
+			{/* single question */}
+			<div className='question'>
+				<h4>Different Payment Methods</h4>
+				<div className='answer'>
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque,
+						in.
+					</p>
+				</div>
+				<div className='faq-line'></div>
+			</div>
+			{/* end of single question */}
+			{/* single question */}
+			<div className='question'>
+				<h4>What Products Do You Offer?</h4>
+				<div className='answer'>
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque,
+						in.
+					</p>
+				</div>
+				<div className='faq-line'></div>
+			</div>
+			{/* end of single question */}
+		</Faq>
+	);
 }
+
+const Faq = styled(About)`
+  display: block;
+  span{
+    display: block;
+  }
+  h2{
+    padding-bottom: 2rem;
+  }
+  .faq-line{
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question{
+    padding: 3rem;
+    cursor: pointer;
+  }
+  .answer{
+    padding: 2rem;
+    p{
+      padding: 1rem 0rem;
+    }
+  }
+`
 
 export default FaqSection
